ebf2b6a57c51f231cdd583baa7e51ae6942e61e4
Reindra12 <reindrairawan@gmail.com>
2020-02-19 01:15:16 +0700

    aksi onclick pada button

--- app/src/main/java/com/reindra/simplenotif/MainActivity.java
+++ app/src/main/java/com/reindra/simplenotif/MainActivity.java
@@ -12,3 +12,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 }
+    //aksi onclik button
+        public void sendNotification(View view){
+
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://dicoding.com"));
+            PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
+            NotificationManager mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
