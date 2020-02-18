847a7db651950726980ccf476be2a21b33172c34
Reindra12 <reindrairawan@gmail.com>
2020-02-19 01:15:40 +0700

    untuk menampilkan notifikasi

--- app/src/main/java/com/reindra/simplenotif/MainActivity.java
+++ app/src/main/java/com/reindra/simplenotif/MainActivity.java
@@ -18,3 +18,11 @@ public class MainActivity extends AppCompatActivity {
             Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://dicoding.com"));
             PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
             NotificationManager mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+            NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                    .setContentIntent(pendingIntent)
+                    .setSmallIcon(R.drawable.baseline_notifications_white_18dp)
+                    .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.drawable.baseline_notifications_white_18dp))
+                    .setContentTitle(getResources().getString(R.string.content_title))
+                    .setContentText(getResources().getString(R.string.content_text))
+                    .setSubText(getResources().getString(R.string.subtext))
+                    .setAutoCancel(true);
